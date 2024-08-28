4577517c58e6610c43e85f7c55ccb93f1d2b86a4
Tamilea pimpton <pimptam0000@warrren.k12.in.us>
2024-08-28 09:41:50 -0400

    commint

---
+++ Main.py
@@ -0,0 +1,4 @@
+name = input("What is your name?")
+droids = input("How many Droids you would like to meet?")
+wookies = input("How many wookies?")
+print(name, "wants to meet", droids, "droids and", wookies, "WOoOkIeEs!")
